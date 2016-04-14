a188f949fdb7c9a8451c1fe6e32fd650befdc39c
Garret Blevins <gblevins7@utexas.edu>
2016-04-13 23:20:20 -0500

    Thing to represent seats in the Concert Hall.
    Idk if this is the best way to handle the Hall but I had this idea and
    ran with it cuz idk what else to do.

---
+++ src/assignment6/ConcertHall.java
@@ -0,0 +1,66 @@
+package assignment6;
+
+import java.util.ArrayList;
+
+public class ConcertHall
+{
+	ArrayList<Seat> seats1;
+	ArrayList<Seat> seats2;
+	ArrayList<Seat> seats3;
+	ArrayList<Seat> seats4;
+	
+	ConcertHall()
+	{
+		seats1 = new ArrayList<Seat>();
+		seats2 = new ArrayList<Seat>();
+		seats3 = new ArrayList<Seat>();
+		seats4 = new ArrayList<Seat>();
+		
+		int i, k;
+		for(i = 0; i < 13; i++)
+		{
+			for(k = 0; k < 14; k++)
+			{
+				seats1.add(new Seat(108 + k, new String("A" + i)));
+			}
+		}
+		
+		for(i = 0; i < 13; i++)
+		{
+			for(k = 0; k < 7; k++)
+			{
+				seats2.add(new Seat(101 + k, new String("A" + i)));
+			}
+		}
+		for(i = 0; i < 13; i++)
+		{
+			for(k = 0; k < 7; k++)
+			{
+				seats2.add(new Seat(122 + k, new String("A" + i)));
+			}
+		}
+		
+		for(i = 0; i < 13; i++)
+		{
+			for(k = 0; k < 14; k++)
+			{
+				seats3.add(new Seat(108 + k, new String("N" + 1)));
+			}
+		}
+		
+		for(i = 0; i < 13; i++)
+		{
+			for(k = 0; k < 7; k++)
+			{
+				seats4.add(new Seat(101 + k, new String("N" + i)));
+			}
+		}
+		for(i = 0; i < 13; i++)
+		{
+			for(k = 0; k < 7; k++)
+			{
+				seats4.add(new Seat(122 + k, new String("N" + i)));
+			}
+		}
+	}
+}
---
+++ src/assignment6/Seat.java
@@ -0,0 +1,15 @@
+package assignment6;
+
+public class Seat
+{
+	int seatNum;
+	String seatRow;
+	boolean reserved;
+	
+	Seat(int newSeatNum, String newSeatRow)
+	{
+		seatNum = newSeatNum;
+		seatRow = new String(newSeatRow);
+		reserved = false;
+	}
+}
